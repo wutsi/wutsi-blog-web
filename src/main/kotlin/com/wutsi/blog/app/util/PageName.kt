8ac14e5df0c5375c24e563939ea124343fdfc4f0
htchepannou <htchepannou@expedia.com>
2020-03-31 14:49:10 -0400

    hide user picture in login page

--- src/main/kotlin/com/wutsi/blog/app/util/PageName.kt
+++ src/main/kotlin/com/wutsi/blog/app/util/PageName.kt
@@ -2,20 +2,20 @@ package com.wutsi.blog.app.util
 
 class PageName{
     companion object {
-        const val HOME = "pageName.home"
-        const val LOGIN = "pageName.login"
-        const val LOGOUT = "pageName.logout"
-        const val STORY_EDITOR = "pageName.story.editor"
-        const val STORY_LIST_DRAFT = "pageName.story.list.draft"
-        const val STORY_LIST_PUBLISHED = "pageName.story.list.published"
-        const val STORY_PUBLISH = "pageName.story.publish"
-        const val STORY_CONFIRMATION = "pageName.story.confirmation"
-        const val STORY_SHARE = "pageName.story.share"
-        const val READ = "pageName.read"
+        const val HOME = "page.home"
+        const val LOGIN = "page.login"
+        const val LOGOUT = "page.logout"
+        const val STORY_EDITOR = "page.story.editor"
+        const val STORY_LIST_DRAFT = "page.story.list.draft"
+        const val STORY_LIST_PUBLISHED = "page.story.list.published"
+        const val STORY_PUBLISH = "page.story.publish"
+        const val STORY_CONFIRMATION = "page.story.confirmation"
+        const val STORY_SHARE = "page.story.share"
+        const val READ = "page.read"
 
-        const val ERROR_400 = "pageName.error.400"
-        const val ERROR_403 = "pageName.error.403"
-        const val ERROR_404 = "pageName.error.404"
-        const val ERROR_500 = "pageName.error.500"
+        const val ERROR_400 = "page.error.400"
+        const val ERROR_403 = "page.error.403"
+        const val ERROR_404 = "page.error.404"
+        const val ERROR_500 = "page.error.500"
     }
 }
