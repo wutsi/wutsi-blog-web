f15ae8362a8f7d5438d7125cd003e6e739b4a359
htchepannou <htchepannou@expedia.com>
2020-04-27 00:59:22 -0400

    disable caching for QA

--- src/main/kotlin/com/wutsi/blog/app/config/CacheConfiguration.kt
+++ src/main/kotlin/com/wutsi/blog/app/config/CacheConfiguration.kt
@@ -2,15 +2,24 @@ package com.wutsi.blog.app.config
 
 import com.wutsi.core.cache.CacheService
 import com.wutsi.core.cache.HashMapCacheService
+import com.wutsi.core.cache.NullCacheService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
 class CacheConfiguration{
 
     @Bean
+    @Profile("!qa")
     fun cacheService(): CacheService {
         return HashMapCacheService()
     }
 
+
+    @Bean
+    @Profile("qa")
+    fun qaCacheService(): CacheService {
+        return NullCacheService()
+    }
 }
