24f637aaea46286ec36c6b472be00b9a32176ebc
htchepannou <htchepannou@expedia.com>
2020-05-12 18:51:56 -0400

    fix build

--- src/main/kotlin/com/wutsi/blog/app/mapper/StoryMapper.kt
+++ src/main/kotlin/com/wutsi/blog/app/mapper/StoryMapper.kt
@@ -45,7 +45,7 @@ class StoryMapper(
                 published = story.status == StoryStatus.published,
                 modificationDateTime = moment.format(story.modificationDateTime),
                 creationDateTime = moment.format(story.creationDateTime),
-                publishedDateTime = story.publishedDateTime,
+                publishedDateTimeAsDate = story.publishedDateTime,
                 publishedDateTime = moment.format(story.publishedDateTime),
                 publishedDateTimeISO8601 = if (story.publishedDateTime == null) null else fmt.format(story.publishedDateTime),
                 modificationDateTimeISO8601 = fmt.format(story.modificationDateTime),
