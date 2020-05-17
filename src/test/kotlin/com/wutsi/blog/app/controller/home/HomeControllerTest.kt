36307845df8fdad33259343aac9ab6304670d3e7
htchepannou <htchepannou@expedia.com>
2020-05-17 09:26:44 -0400

    fix test

--- src/test/kotlin/com/wutsi/blog/app/controller/home/HomeControllerTest.kt
+++ src/test/kotlin/com/wutsi/blog/app/controller/home/HomeControllerTest.kt
@@ -2,36 +2,31 @@ package com.wutsi.blog.app.controller.home
 
 import com.wutsi.blog.SeleniumTestSupport
 import com.wutsi.blog.app.util.PageName
-import org.junit.Before
 import org.junit.Test
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 
 
 class HomeControllerTest: SeleniumTestSupport() {
-    @Before
-    override fun setUp(){
-        super.setUp()
-
-        driver.get("$url")
-    }
-
-
     @Test
     fun `empty home page`() {
         stub(HttpMethod.POST, "/v1/story/search", HttpStatus.OK, "v1/story/search-empty.json")
+        driver.get("$url")
+
         assertCurrentPageIs(PageName.HOME)
         assertElementCount(".post", 0)
     }
 
     @Test
     fun `user should view recent stories in home page`() {
+        driver.get("$url")
         assertCurrentPageIs(PageName.HOME)
         assertElementCount(".post", 4)
     }
 
     @Test
     fun `home page should contains META headers`() {
+        driver.get("$url")
         assertElementAttribute("head title", "text", META_TITLE)
         assertElementPresent("head meta[name='description']")
         assertElementAttribute("head meta[name='robots']", "content", "all")
