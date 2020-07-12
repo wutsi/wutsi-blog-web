1bd557b25ca0cd02f86beae3345343874c33c6aa
htchepannou <htchepannou@expedia.com>
2020-07-12 13:26:13 -0400

    fix twitter authentication

--- src/main/kotlin/com/wutsi/blog/app/security/TwitterLoginController.kt
+++ src/main/kotlin/com/wutsi/blog/app/security/TwitterLoginController.kt
@@ -1,5 +1,7 @@
 package com.wutsi.blog.app.security
 
+import com.github.scribejava.core.model.OAuthRequest
+import com.github.scribejava.core.model.Verb
 import com.github.scribejava.core.oauth.OAuth10aService
 import com.wutsi.blog.app.security.config.OAuthConfiguration
 import com.wutsi.core.logging.KVLogger
@@ -18,7 +20,7 @@ import javax.servlet.http.HttpServletRequest
 @RequestMapping("/login/twitter")
 class TwitterLoginController(
         @Qualifier(OAuthConfiguration.TWITTER_OAUTH_SERVICE) private val oauth: OAuth10aService,
-        logger: KVLogger
+        private val logger: KVLogger
 ) {
     @GetMapping()
     fun login (request: HttpServletRequest): String {
@@ -28,7 +30,16 @@ class TwitterLoginController(
     }
 
     @GetMapping("/callback")
-    open fun callback(request: HttpServletRequest): String {
+    fun callback(request: HttpServletRequest): String {
+        val requestToken = oauth.getRequestToken()
+        val accessToken = oauth.getAccessToken(requestToken, "123")
+
+        val request = OAuthRequest(Verb.GET, "https://api.twitter.com/1.1/account/verify_credentials.json")
+        oauth.signRequest(accessToken, request)
+
+        val response = oauth.execute(request)
+        logger.add("response", response.body)
+
         return ""
     }
 }
