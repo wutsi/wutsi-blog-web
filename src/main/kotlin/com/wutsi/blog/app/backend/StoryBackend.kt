c09909433f599cccfe1a246421e91e817001bcda
htchepannou <htchepannou@expedia.com>
2020-08-23 00:41:39 -0400

    remove cache

--- src/main/kotlin/com/wutsi/blog/app/backend/StoryBackend.kt
+++ src/main/kotlin/com/wutsi/blog/app/backend/StoryBackend.kt
@@ -1,6 +1,5 @@
 package com.wutsi.blog.app.backend
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.blog.client.story.CountStoryResponse
 import com.wutsi.blog.client.story.GetStoryReadabilityResponse
 import com.wutsi.blog.client.story.GetStoryResponse
@@ -12,21 +11,13 @@ import com.wutsi.blog.client.story.SaveStoryRequest
 import com.wutsi.blog.client.story.SaveStoryResponse
 import com.wutsi.blog.client.story.SearchStoryRequest
 import com.wutsi.blog.client.story.SearchStoryResponse
-import com.wutsi.blog.client.story.StoryDto
-import com.wutsi.blog.client.story.StoryStatus
-import com.wutsi.core.cache.CacheService
 import com.wutsi.core.http.Http
-import com.wutsi.core.logging.KVLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class StoryBackend (
-        private val http: Http,
-        private val cache: CacheService,
-        private val logger: KVLogger,
-        private val objectMapper: ObjectMapper,
-        @Value("\${wutsi.cache.story-ttl-seconds}") private val storyCacheTTL: Int
+        private val http: Http
 ) {
     @Value("\${wutsi.backend.story.endpoint}")
     private lateinit var endpoint: String
@@ -36,20 +27,11 @@ class StoryBackend (
     }
 
     fun update(id: Long, request: SaveStoryRequest): SaveStoryResponse {
-        val response = http.post("$endpoint/$id", request, SaveStoryResponse::class.java).body!!
-        deleteFromCache(id)
-        return response
+        return http.post("$endpoint/$id", request, SaveStoryResponse::class.java).body!!
     }
 
     fun get(id:Long): GetStoryResponse {
-        var response = getFromCache(id)
-        if (response != null) {
-            return response
-        }
-
-        response = http.get("$endpoint/$id", GetStoryResponse::class.java).body!!
-        putToCache(response.story)
-        return response
+        return http.get("$endpoint/$id", GetStoryResponse::class.java).body!!
     }
 
     fun readability(id:Long): GetStoryReadabilityResponse {
@@ -65,75 +47,10 @@ class StoryBackend (
     }
 
     fun publish(id:Long, request: PublishStoryRequest): PublishStoryResponse {
-        val response = http.post("$endpoint/$id/publish", request, PublishStoryResponse::class.java).body!!
-        deleteFromCache(id)
-        return response
+        return http.post("$endpoint/$id/publish", request, PublishStoryResponse::class.java).body!!
     }
 
     fun import(request: ImportStoryRequest): ImportStoryResponse {
         return http.post("$endpoint/import", request, ImportStoryResponse::class.java).body!!
-    }
-
-    fun getFromCache(id: Long): GetStoryResponse? {
-        var exception: Exception? = null
-        val key = cacheKey(id)
-        var success = false
-
-        try {
-            val json = cache.get(key)
-            if (json != null){
-                val story = objectMapper.readValue(json, StoryDto::class.java)
-                success = true
-                return GetStoryResponse(story = story)
-            }
-
-        } catch (ex: Exception) {
-            exception = ex
-        } finally {
-            logCache("GET", key, success, exception)
-        }
-        return null
-    }
-
-    fun putToCache(story: StoryDto) {
-        if (story.status != StoryStatus.published || !story.live) {
-            return
-        }
-
-        var exception: Exception? = null
-        val key = cacheKey(story.id)
-
-        try {
-            val value = objectMapper.writeValueAsString(story)
-            cache.put(key, value, storyCacheTTL)
-        } catch (ex: Exception) {
-            exception = ex
-        } finally {
-            logCache("PUT", key, true, exception)
-        }
-    }
-
-    fun deleteFromCache(id: Long) {
-        var exception: Exception? = null
-        val key = cacheKey(id)
-
-        try {
-            cache.remove(key)
-        } catch (ex: Exception) {
-            exception = ex
-        } finally {
-            logCache("DELETE", key, true, exception)
-        }
-    }
-
-    private fun cacheKey(id: Long) = "story.$id"
-
-    private fun logCache(operation: String, key: String, success: Boolean, ex: Exception? = null){
-        logger.add("Cache${operation}Key", key)
-        logger.add("Cache${operation}Success", success && ex == null)
-        if (ex != null) {
-            logger.add("Cache${operation}Exception", ex.javaClass.name)
-            logger.add("Cache${operation}ExceptionMessage", ex.message)
-        }
     }
 }
