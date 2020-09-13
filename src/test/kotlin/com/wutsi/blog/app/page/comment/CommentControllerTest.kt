396574a29c72257264cf56e8728d307fdd1320c3
htchepannou <htchepannou@expedia.com>
2020-09-13 17:56:51 -0400

    fix test

--- src/test/kotlin/com/wutsi/blog/app/page/comment/CommentControllerTest.kt
+++ src/test/kotlin/com/wutsi/blog/app/page/comment/CommentControllerTest.kt
@@ -26,8 +26,14 @@ class CommentControllerTest: SeleniumMobileTestSupport() {
         driver.get(url)
 
         Thread.sleep(1000)
-        assertElementCount(".comment-badge", 1)
+        assertElementCount(".comment-badge", 7)
         assertElementText(".comment-badge #comment-count-20", "3")
+        assertElementText(".comment-badge #comment-count-21", "5")
+        assertElementText(".comment-badge #comment-count-22", "1")
+        assertElementText(".comment-badge #comment-count-23", "")
+        assertElementText(".comment-badge #comment-count-24", "")
+        assertElementText(".comment-badge #comment-count-25", "")
+        assertElementText(".comment-badge #comment-count-26", "")
     }
 
     @Test
