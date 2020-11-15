8c0fd6842ab026dbe9bb437c0fc3c04031bc40aa
htchepannou <htchepannou@expedia.com>
2020-11-14 22:00:11 -0500

    follow

--- src/main/kotlin/com/wutsi/blog/app/backend/FollowerBackend.kt
+++ src/main/kotlin/com/wutsi/blog/app/backend/FollowerBackend.kt
@@ -1,7 +1,9 @@
 package com.wutsi.blog.app.backend
 
-import com.wutsi.blog.client.follow.CreateFollowerRequest
-import com.wutsi.blog.client.follow.CreateFollowerResponse
+import com.wutsi.blog.client.follower.CreateFollowerRequest
+import com.wutsi.blog.client.follower.CreateFollowerResponse
+import com.wutsi.blog.client.follower.SearchFollowerRequest
+import com.wutsi.blog.client.follower.SearchFollowerResponse
 import com.wutsi.core.http.Http
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -11,9 +13,12 @@ class FollowerBackend (private val http: Http) {
     @Value("\${wutsi.backend.follower.endpoint}")
     private lateinit var endpoint: String
 
-    fun create(request: CreateFollowerRequest): CreateFollowerResponse {
-        return http.post(endpoint, request, CreateFollowerResponse::class.java).body!!
-    }
+    fun create(request: CreateFollowerRequest): CreateFollowerResponse =
+        http.post(endpoint, request, CreateFollowerResponse::class.java).body!!
+
+
+    fun search(request: SearchFollowerRequest): SearchFollowerResponse =
+         http.post("$endpoint/search", request, SearchFollowerResponse::class.java).body!!
 
     fun delete(id: Long) {
         http.delete("$endpoint/$id")
--- src/main/kotlin/com/wutsi/blog/app/page/follower/service/FollowerService.kt
+++ src/main/kotlin/com/wutsi/blog/app/page/follower/service/FollowerService.kt
@@ -2,7 +2,8 @@ package com.wutsi.blog.app.page.follower.service
 
 import com.wutsi.blog.app.backend.FollowerBackend
 import com.wutsi.blog.app.common.service.RequestContext
-import com.wutsi.blog.client.follow.CreateFollowerRequest
+import com.wutsi.blog.client.follower.CreateFollowerRequest
+import com.wutsi.blog.client.follower.SearchFollowerRequest
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,13 +13,22 @@ class FollowerService(
 ) {
     fun follow(blogId: Long): Long = backend.create(CreateFollowerRequest(
             userId = blogId,
-            followerId = requestContext.currentUser()?.id
+            followerUserId = requestContext.currentUser()?.id
         )).followerId
 
-    fun findFollwerId(id: Long): Long? =
-            if (requestContext.currentUser() == null)
-                null
-            else
-                if (Math.random() < .5) null else 1L
+    fun findFollwerId(id: Long): Long? {
+        requestContext.currentUser()
+                ?: return null
+
+        try {
+            val followers = backend.search(SearchFollowerRequest(
+                    userId = id,
+                    followerUserId = requestContext.currentUser()?.id
+            )).followers
+            return if (followers.size == 0) null else followers[0].id
+        }catch(ex: Exception) {
+            return null
+        }
+    }
 
 }
