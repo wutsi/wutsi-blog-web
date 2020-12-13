4bc641c8afb82afaead66977e4541044e5b4958a
htchepannou <htchepannou@expedia.com>
2020-12-13 10:09:37 -0500

    remove needless blank like

--- src/main/kotlin/com/wutsi/blog/app/page/sitemap/model/SitemapModel.kt
+++ src/main/kotlin/com/wutsi/blog/app/page/sitemap/model/SitemapModel.kt
@@ -5,7 +5,6 @@ import javax.xml.bind.annotation.XmlAccessorType
 import javax.xml.bind.annotation.XmlElement
 import javax.xml.bind.annotation.XmlRootElement
 
-
 @XmlRootElement(name = "urlset", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
 @XmlAccessorType(XmlAccessType.FIELD)
 data class SitemapModel(
