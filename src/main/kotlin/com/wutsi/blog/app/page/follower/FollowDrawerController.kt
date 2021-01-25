5bf4846112b67599bc06ed42513efa6d10e3a988
htchepannou <htchepannou@expedia.com>
2021-01-25 18:07:49 -0500

    improve the impression of the follow popup

--- src/main/kotlin/com/wutsi/blog/app/page/follower/FollowDrawerController.kt
+++ src/main/kotlin/com/wutsi/blog/app/page/follower/FollowDrawerController.kt
@@ -4,8 +4,8 @@ import com.wutsi.blog.app.common.service.RequestContext
 import com.wutsi.blog.app.common.service.ViewService
 import com.wutsi.blog.app.page.follower.service.FollowerService
 import com.wutsi.blog.app.page.settings.service.UserService
-import com.wutsi.blog.app.page.story.model.StoryModel
-import com.wutsi.blog.app.page.story.service.StoryService
+import com.wutsi.blog.app.util.CookieHelper
+import com.wutsi.blog.app.util.CookieName
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,39 +18,68 @@ class FollowDrawerController(
     private val viewService: ViewService,
     private val followService: FollowerService,
     private val userService: UserService,
-    private val storyService: StoryService,
     private val requestContext: RequestContext
 ) {
     companion object {
         private val MAX_MONTHLY_FREE_VIEWED_STORIES = 3
+        private val MAX_DAILY_IMPRESSION_COUNT = 3
     }
 
     @GetMapping()
     fun index(
         @RequestParam userId: Long,
-        @RequestParam storyId: Long,
+        @RequestParam storyUrl: String,
         model: Model
     ): String {
-        val story = storyService.get(storyId)
-        if (shouldShow(userId, story)) {
+        if (shouldShow(userId)) {
             val user = userService.get(userId)
+            trackImpression()
+
             model.addAttribute("show", true)
             model.addAttribute("user", user)
             model.addAttribute("followUrl", "${user.slug}/follow")
-            model.addAttribute("storyUrl", story.slug)
+            model.addAttribute("storyUrl", storyUrl)
         }
         return "page/follow/drawer"
     }
 
-    private fun shouldShow(userId: Long, story: StoryModel): Boolean {
+    private fun shouldShow(userId: Long): Boolean {
         if (
-            !requestContext.toggles().followDrawer ||
+            impressionCount() >= MAX_DAILY_IMPRESSION_COUNT || /* Not too many impressions */
+            !requestContext.toggles().followDrawer || /* Toggles enabled */
             !requestContext.toggles().follow ||
-            !followService.canFollow(userId)
+            !isLoggedIn() || /* user not logged in */
+            !followService.canFollow(userId) /* user cannot follow the blog */
         )
             return false
 
         val storyIds = viewService.storiesViewedThisMonth()
         return storyIds.size >= MAX_MONTHLY_FREE_VIEWED_STORIES
     }
+
+    private fun isLoggedIn(): Boolean =
+        requestContext.currentUser() != null
+
+    fun impressionCount(): Int {
+        val value = CookieHelper.get(impressionKey(), requestContext.request)
+            ?: return 0
+        try {
+            return value.toInt()
+        } catch (ex: Exception) {
+            return 0
+        }
+    }
+
+    private fun trackImpression() {
+        CookieHelper.put(
+            name = impressionKey(),
+            value = (impressionCount() + 1).toString(),
+            request = requestContext.request,
+            response = requestContext.response,
+            maxAge = CookieHelper.ONE_DAY_SECONDS
+        )
+    }
+
+    private fun impressionKey(): String =
+        CookieName.FOLLOW_DRAWER
 }
--- src/main/kotlin/com/wutsi/blog/app/util/CookieName.kt
+++ src/main/kotlin/com/wutsi/blog/app/util/CookieName.kt
@@ -8,5 +8,6 @@ class CookieName {
         const val REFERER = "__w_rfr"
         const val LOCALE = "__w_loc"
         const val DEVICE_UID = DeviceUIDProvider.COOKIE_NAME
+        const val FOLLOW_DRAWER = "__w_fd"
     }
 }
