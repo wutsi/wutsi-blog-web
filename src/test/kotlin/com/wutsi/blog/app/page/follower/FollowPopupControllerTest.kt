e60db6a212d732a8555f146e7123e8d0b4acee7a
htchepannou <htchepannou@expedia.com>
2021-01-26 14:25:11 -0500

    add tests

--- src/test/kotlin/com/wutsi/blog/app/page/follower/FollowPopupControllerTest.kt
+++ src/test/kotlin/com/wutsi/blog/app/page/follower/FollowPopupControllerTest.kt
@@ -1,6 +1,10 @@
 package com.wutsi.blog.app.page.follower
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.blog.SeleniumMobileTestSupport
+import com.wutsi.blog.fixtures.FollowerApiFixtures
 import org.junit.Test
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -17,17 +21,43 @@ class FollowPopupControllerTest : SeleniumMobileTestSupport() {
     fun `anonymous see see popup`() {
         driver.get("$url/read/20/test")
 
-        verifyDrawer()
+        verifyNoDrawer()
     }
 
     @Test
-    fun `non-subscriber can see popup`() {
+    fun `non-subscriber can see popup and follow`() {
+        val response = FollowerApiFixtures.createSearchFollowerResponse(1L)
+        doReturn(response).whenever(followerApi).create(any())
+
         login()
 
         stub(HttpMethod.GET, "/v1/story/20", HttpStatus.OK, "v1/story/get-story20-user99.json")
         driver.get("$url/read/20/test")
 
         verifyDrawer()
+
+        click("#follow-popup .btn-follow")
+        Thread.sleep(1000)
+        assertElementNotVisible("#follow-popup")
+        assertElementNotVisible(".navbar .btn-follow")
+        assertElementNotVisible(".follow-panel")
+    }
+
+    @Test
+    fun `non-subscriber can see popup and close`() {
+        login()
+
+        stub(HttpMethod.GET, "/v1/story/20", HttpStatus.OK, "v1/story/get-story20-user99.json")
+        driver.get("$url/read/20/test")
+
+        verifyDrawer()
+
+        click("#follow-popup .btn-close")
+
+        Thread.sleep(1000)
+        assertElementNotVisible("#follow-popup")
+        assertElementVisible(".navbar .btn-follow")
+        assertElementVisible(".follow-panel")
     }
 
     @Test
@@ -54,6 +84,8 @@ class FollowPopupControllerTest : SeleniumMobileTestSupport() {
         Thread.sleep(1000)
 
         assertElementVisible("#follow-popup")
+        assertElementAttribute("#follow-popup .btn-follow", "wutsi-track-event", "follow_popup_click")
+        assertElementAttribute("#follow-popup .btn-close", "wutsi-track-event", "follow_popup_close")
     }
 
     private fun verifyNoDrawer() {
--- src/test/kotlin/com/wutsi/blog/fixtures/FollowerApiFixtures.kt
+++ src/test/kotlin/com/wutsi/blog/fixtures/FollowerApiFixtures.kt
@@ -1,5 +1,6 @@
 package com.wutsi.blog.fixtures
 
+import com.wutsi.blog.client.follower.CreateFollowerResponse
 import com.wutsi.blog.client.follower.FollowerDto
 import com.wutsi.blog.client.follower.SearchFollowerResponse
 
@@ -12,5 +13,9 @@ object FollowerApiFixtures {
 
     fun createSearchFollowerResponse(userId: Long, followerUserId: Long) = SearchFollowerResponse(
         followers = listOf(createFolloweDto(userId, followerUserId))
+    )
+
+    fun createSearchFollowerResponse(followerId: Long) = CreateFollowerResponse(
+        followerId = followerId
     )
 }
