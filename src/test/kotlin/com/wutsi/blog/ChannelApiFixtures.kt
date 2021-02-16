7fdd11cad81b8d9167cf4bf01be2e891dcfee14a
htchepannou <htchepannou@expedia.com>
2021-02-15 22:07:52 -0500

    fix klint issues

--- src/test/kotlin/com/wutsi/blog/ChannelApiFixtures.kt
+++ src/test/kotlin/com/wutsi/blog/ChannelApiFixtures.kt
@@ -2,21 +2,19 @@ package com.wutsi.blog
 
 import com.wutsi.blog.client.channel.ChannelDto
 import com.wutsi.blog.client.channel.ChannelType
-import com.wutsi.blog.client.pin.GetPinResponse
-import com.wutsi.blog.client.pin.PinDto
 import java.util.UUID
 
 object ChannelApiFixtures {
     fun createChannelDto(
-            userId:Long,
-            type: ChannelType,
-            pictureUrl: String = "https://avatars3.githubusercontent.com/u/39621277?v=4"
+        userId: Long,
+        type: ChannelType,
+        pictureUrl: String = "https://avatars3.githubusercontent.com/u/39621277?v=4"
     ) = ChannelDto(
-            id = System.currentTimeMillis(),
-            userId = userId,
-            type = type,
-            pictureUrl = pictureUrl,
-            name = "Name $userId",
-            providerUserId = UUID.randomUUID().toString()
+        id = System.currentTimeMillis(),
+        userId = userId,
+        type = type,
+        pictureUrl = pictureUrl,
+        name = "Name $userId",
+        providerUserId = UUID.randomUUID().toString()
     )
 }
