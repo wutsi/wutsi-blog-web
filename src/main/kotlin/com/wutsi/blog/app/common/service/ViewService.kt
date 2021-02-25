c016012f6985c599c13cb512d9dcdd06ba098631
htchepannou <htchepannou@expedia.com>
2021-02-25 15:21:12 -0500

    fix test

--- src/main/kotlin/com/wutsi/blog/app/common/service/ViewService.kt
+++ src/main/kotlin/com/wutsi/blog/app/common/service/ViewService.kt
@@ -7,6 +7,7 @@ import com.wutsi.blog.client.user.SearchUserRequest
 import com.wutsi.blog.client.view.SearchPreferredAuthorRequest
 import com.wutsi.blog.client.view.SearchViewRequest
 import org.apache.commons.lang.time.DateUtils
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.Date
 
@@ -16,17 +17,26 @@ class ViewService(
     private val requestContext: RequestContext,
     private val userService: UserService
 ) {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(ViewService::class.java)
+    }
+
     fun findViewedStoryIdsLastWeek(): Collection<Long> {
-        val now = Date()
-        val views = viewBackend.search(
-            SearchViewRequest(
-                userId = requestContext.currentUser()?.id,
-                deviceId = requestContext.deviceId(),
-                viewStartDate = DateUtils.addDays(now, -7),
-                viewEndDate = now
-            )
-        ).views
-        return views.map { it.storyId }.toSet()
+        try {
+            val now = Date()
+            val views = viewBackend.search(
+                SearchViewRequest(
+                    userId = requestContext.currentUser()?.id,
+                    deviceId = requestContext.deviceId(),
+                    viewStartDate = DateUtils.addDays(now, -7),
+                    viewEndDate = now
+                )
+            ).views
+            return views.map { it.storyId }.toSet()
+        } catch (ex: Exception) {
+            LOGGER.error("Unable to load viewed stories", ex)
+            return emptyList()
+        }
     }
 
     fun findPreferredAuthors(limit: Int = 3): List<UserModel> {
