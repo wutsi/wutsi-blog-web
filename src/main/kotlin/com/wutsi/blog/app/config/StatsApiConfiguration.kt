cebddfee40fdfb8d9cd2b998945d5d678d116c2e
htchepannou <htchepannou@expedia.com>
2021-04-23 19:53:56 -0400

    fix serialization of stat

--- src/main/kotlin/com/wutsi/blog/app/config/StatsApiConfiguration.kt
+++ src/main/kotlin/com/wutsi/blog/app/config/StatsApiConfiguration.kt
@@ -1,8 +1,11 @@
 package com.wutsi.blog.app.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import com.wutsi.stats.StatsApi
 import com.wutsi.stats.StatsApiBuilder
+import com.wutsi.stream.ObjectMapperBuilder
 import feign.RequestInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -13,17 +16,22 @@ import org.springframework.core.env.Profiles
 @Configuration
 class StatsApiConfiguration(
     @Autowired private val env: Environment,
-    @Autowired private val mapper: ObjectMapper,
     @Autowired private val tracingRequestInterceptor: RequestInterceptor
 ) {
     @Bean
-    fun statsApi(): StatsApi =
-        StatsApiBuilder()
+    fun statsApi(): StatsApi {
+        val mapper = ObjectMapperBuilder().build()
+        mapper.registerModule(ParameterNamesModule())
+        mapper.registerModule(Jdk8Module())
+        mapper.registerModule(JavaTimeModule())
+
+        return StatsApiBuilder()
             .build(
                 env = statsEnvironment(),
                 mapper = mapper,
                 interceptors = listOf(tracingRequestInterceptor)
             )
+    }
 
     fun statsEnvironment(): com.wutsi.stats.Environment =
         if (env.acceptsProfiles(Profiles.of("prod")))
